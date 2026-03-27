379bc99f05d5f59bba48bc1f30a812f15121d1ea
moisesx04 <moisesx04@gmail.com>
2026-03-27 09:17:33 -0400

    Fix: rutas y dependencias para asegurar carga en producción

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-import React, { StrictMode } from 'react';
+import * as React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SearchProvider } from './context/SearchContext';
